38e9db0bea9bde8bde821c5d55de53254e135ada
ponurakk <p0unrakg@int.pl>
2025-01-01 22:31:54 +0100

    feat: Add leaflet map

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,37 @@
+use std::env::current_dir;
+
+use chrono::Local;
+use gbrouting::csa::{gtfs::parse_gtfs, routing::ConnectionScanCore};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_stops() {
+    // println!("{:#?}", current_dir());
+    let gtfs = gbrouting::gtfs::load("../gtfs/pkpic.zip").unwrap();
+    let cs_data = parse_gtfs(
+        &gtfs,
+        Local::now().naive_local().date(),
+        true,
+        1000.0,
+        0.6,
+        false,
+    )
+    .unwrap();
+
+    let cs_core = ConnectionScanCore::new(&cs_data);
+    // println!("{:#?}", cs_core.get_stops());
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_stops])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -3,12 +3,20 @@
 use std::fmt::Display;
 
 use dioxus::prelude::*;
+use dioxus_logger::tracing::info;
+use leaflet::{Icon, IconOptions};
 use wasm_bindgen::prelude::*;
 
 const STYLES_URL: Asset = asset!("/assets/styles.css");
+const MAP_URL: Asset = asset!("/assets/map.js");
+const LEAFLET_STYLE_URL: Asset = asset!("/assets/leaflet.css");
+const LEAFLET_SCRIPT_URL: Asset = asset!("/assets/leaflet.js");
 
 #[wasm_bindgen]
 extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = invoke)]
+    async fn invoke_without_args(cmd: &str) -> JsValue;
+
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
@@ -58,17 +66,46 @@ fn Counter() -> Element {
     }
 }
 
+fn add_map_to_container(id: &str) {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let container: web_sys::HtmlElement =
+        document.get_element_by_id(id).unwrap().dyn_into().unwrap();
+    let map = leaflet::Map::new_with_element(&container, &leaflet::MapOptions::default());
+    leaflet::TileLayer::new("https://tile.openstreetmap.org/{z}/{x}/{y}.png").add_to(&map);
+    map.set_view(&leaflet::LatLng::new(50.03719, 22.00424), 14.0);
+    leaflet::Marker::new(&leaflet::LatLng::new(50.03719, 22.00424)).add_to(&map);
+}
+
 pub fn App() -> Element {
+    use_effect(move || {
+        let window = web_sys::window().unwrap();
+        let closure = Closure::wrap(Box::new(move || {
+            add_map_to_container("map");
+        }) as Box<dyn Fn()>);
+
+        // Defer execution so js has time to load
+        window
+            .set_timeout_with_callback_and_timeout_and_arguments_0(
+                closure.as_ref().unchecked_ref(),
+                100,
+            )
+            .unwrap();
+
+        closure.forget(); // Prevent closure from being dropped
+    });
+
     rsx! {
         document::Link { rel: "stylesheet", href: STYLES_URL }
+        document::Link { rel: "stylesheet", href: LEAFLET_STYLE_URL }
+        document::Script { src: LEAFLET_SCRIPT_URL }
         main {
+            class: "bg-black text-white w-screen h-screen",
+
             h1 {
                 class: "m-0 w-max",
                 "Hello World"
             }
-            Counter {}
-            Counter {}
-            Counter {}
+            div { id: "map", class: "w-screen h-screen" }
         }
     }
 }
