3fc776d462014efdf3c97449686474851456e1db
ponurakk <p0unrakg@int.pl>
2025-01-12 21:27:05 +0100

    feat: Update compartment styles

--- src/components/compartment.rs
+++ src/components/compartment.rs
@@ -10,16 +10,44 @@ pub fn Row(row: Vec<Seat>, number: u8) -> Element {
             for stop in row {
                 if stop.is_occupied {
                     div {
-                        class: "w-10 h-10 bg-red-900 rounded text-white flex items-center align-middle inline-block",
-                        span {
-                            "{number}{stop.number}"
+                        class: "w-12 h-10 m-[1px] bg-red-900 rounded text-white flex items-center align-middle inline-block border-2 border-black",
+                        if stop.number % 2 == 1{
+                            div {
+                                class: "ml-1 w-10 h-10 bg-red-900 rounded text-white flex items-center align-middle inline-block border-2 border-black",
+                                p {
+                                    class: "text-l m-[25%]",
+                                    "{number}{stop.number}"
+                                }
+                            }
+                        } else {
+                            div {
+                                class: "w-10 h-10 bg-red-900 rounded text-white flex items-center align-middle inline-block border-2 border-black",
+                                p {
+                                    class: "text-l m-[25%]",
+                                    "{number}{stop.number}"
+                                }
+                            }
                         }
                     }
                 } else {
                     div {
-                        class: "w-10 h-10 bg-black rounded text-white flex items-center align-middle inline-block",
-                        span {
-                            "{number}{stop.number}"
+                        class: "w-12 h-10 m-[1px] bg-gray-800 rounded text-white flex items-center align-middle inline-block border-2 border-black",
+                        if stop.number % 2 == 1{
+                            div {
+                                class: "ml-1 w-10 h-10 bg-gray-800 rounded text-white flex items-center align-middle inline-block border-2 border-black",
+                                span {
+                                    class: "text-l m-[25%]",
+                                    "{number}{stop.number}"
+                                }
+                            }
+                        } else {
+                            div {
+                                class: "w-10 h-10 bg-gray-800 rounded text-white flex items-center align-middle inline-block border-2 border-black",
+                                span {
+                                    class: "text-l m-[25%]",
+                                    "{number}{stop.number}"
+                                }
+                            }
                         }
                     }
                 }
@@ -33,13 +61,19 @@ pub fn Compartment(compartment: CompartmentType) -> Element {
     rsx! {
         match compartment {
             CompartmentType::Seat6(compartment) => rsx! {
-                for row in compartment.rows {
-                    Row { row, number: compartment.number }
+                div{
+                    class: "p-1 rounded border-2 border-gray-800",
+                    for row in compartment.rows {
+                        Row { row, number: compartment.number }
+                    }
                 }
             },
             CompartmentType::Seat8(compartment) => rsx! {
-                for row in compartment.rows {
-                    Row { row, number: compartment.number }
+                div{
+                    class: "p-1 rounded border-2 border-gray-800",
+                    for row in compartment.rows {
+                        Row { row, number: compartment.number }
+                    }
                 }
             },
             CompartmentType::OpenSpace(compartment) => rsx! {
